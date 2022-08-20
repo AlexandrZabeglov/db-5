814999eb381077d1d58a6b03b49ae2f37fa1bd3c
Alexandr Zabegalov <alexandrzabegalov@mail.ru>
2022-08-20 17:00:37 +0300

    Commit 2.0

---
+++ dirty_main.py
@@ -0,0 +1,36 @@
+import psycopg2
+
+
+def create_db(conn):
+    with conn.cursor() as cur:
+        pass
+
+
+def add_client(conn, first_name, last_name, email, phones=None):
+    pass
+
+
+def add_phone(conn, client_id, phone):
+    pass
+
+
+def change_client(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
+    pass
+
+
+def delete_phone(conn, client_id, phone):
+    pass
+
+
+def delete_client(conn, client_id):
+    pass
+
+
+def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
+    pass
+
+
+with psycopg2.connect(database="clients_db", user="postgres", password="Zabegalov1") as conn:
+    pass  # вызывайте функции здесь
+
+conn.close()
